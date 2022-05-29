de9dca440931162b4216cacfb6c78146ab912389
Evgeniy <evgeni.moskvin@gmail.com>
2022-05-29 16:01:39 +0300

    message corrected, check + format

--- main.py
+++ main.py
@@ -12,20 +12,23 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет, {message.from_user.first_name} {message.from_user.last_name} Что бы получить ссылку для отправки сообщения в WhatsApp без добавления в список контактов, просто пришлите номер в международном формате (например +7(ХХХ)ХХХ-ХХ-ХХ, +7ХХХХХХХХХХ и т.п.):'
+    mess = f'Привет, {message.from_user.first_name} {message.from_user.last_name}! Что бы получить ссылку для отправки сообщения в WhatsApp без добавления в список контактов, просто пришлите номер в международном формате (например +7(ХХХ)ХХХ-ХХ-ХХ, +7ХХХХХХХХХХ):'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
 @bot.message_handler()
 def get_user_text(message):
     text = message.text
-    number, flag = check_number(text)
-    if not flag:
-        bot.send_message(message.chat.id, f"https://api.whatsapp.com/send?phone={number}", parse_mode='html')
+    if text[0] != '+':
+        bot.send_message(message.chat.id, "Не корректно указан номер")
     else:
-        bot.send_message(message.chat.id, "В номере не может быть букв")
+        number, flag = check_number(text)
+        if not flag:
+            bot.send_message(message.chat.id, f"https://api.whatsapp.com/send?phone={number}", parse_mode='html')
+        else:
+            bot.send_message(message.chat.id, "В номере не может быть букв")
     bot.send_message(message.chat.id,
-                     "Что бы получить ссылку пришлите номер в международном формате (например +7(ХХХ)ХХХ-ХХ-ХХ, +7ХХХХХХХХХХ и т.п.)")
+                     "Что бы получить ссылку пришлите номер в международном формате (например +7(ХХХ)ХХХ-ХХ-ХХ, +7ХХХХХХХХХХ):")
 
 
 bot.polling(none_stop=True)
